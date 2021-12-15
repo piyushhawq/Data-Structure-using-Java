acbd2870f50c4f0c00292ec149de81241a03c2a4
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-15 12:40:42 +0530

    Create Square Pattern.java

---
+++ Patterns 1/Square Pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int i,j,n;  
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();	 
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print(n);
+            }
+            System.out.println();
+        }             
+    }
+}
+
