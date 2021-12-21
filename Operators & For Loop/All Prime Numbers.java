2005fa6d1234f866cca6c66f530f9dfdfc2ef5c9
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-21 12:19:10 +0530

    Create All Prime Numbers.java

---
+++ Operators & For Loop/All Prime Numbers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Solution 
+{
+
+    public static void main(String[] args)
+    {
+
+        /* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int flag;
+        for (int i = 2; i <= n; i++)
+        {
+            if (i == 1 || i == 0)
+                continue;
+            flag = 1;
+
+            for (int j = 2; j <= i / 2; ++j) 
+            {
+                if (i % j == 0) 
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+                System.out.println(i);
+        }
+    }
+}
