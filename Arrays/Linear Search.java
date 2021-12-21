caa76b1483419a7eee7c29d26a8f55718f9a174c
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-21 12:48:54 +0530

    Create Linear Search.java

---
+++ Arrays/Linear Search.java
@@ -0,0 +1,14 @@
+public class Solution 
+{	
+	public static  int linearSearch(int[] arr,int n)
+    {
+		for(int i=0;i<arr.length;i++) 
+        {
+			if(arr[i]==n)
+            {
+				return i;
+			}
+		}
+		return -1;	
+	}
+}
