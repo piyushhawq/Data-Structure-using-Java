4d7a9a5cd9367b4c744126988f9e9cd8f8217854
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-22 22:44:55 +0530

    Create Bubble Sort.java

---
+++ Searching & Sorting/Bubble Sort.java
@@ -0,0 +1,17 @@
+public class Solution 
+{	
+    //arr is the input array you have to sort
+    public static void bubbleSort(int[] arr)
+    {
+        //  int s=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length-1;j++)
+            {if(arr[j]>arr[j+1])
+            {int temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;}
+            }
+        }
+    }
+}
