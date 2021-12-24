2bcf6bb3dc75198d7b2f88516e8b2f79ffe0321a
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-24 23:51:52 +0530

    Create Count Words.java

---
+++ Strings/Count Words.java
@@ -0,0 +1,17 @@
+public class Solution 
+{
+    public static int countWords(String str) 
+    {
+        if(str.length()==0)
+            return 0;
+        int words=1;
+        for(int i=0;i<str.length();i++) 
+        {	
+            if(str.charAt(i)==' ') 
+            {
+                words++;
+            }	
+        }
+        return words;
+    }
+}
