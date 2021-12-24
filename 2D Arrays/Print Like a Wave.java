f448343acb089c04d02e91ab1beaa5ab2c6e5768
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-24 23:46:16 +0530

    Create Print Like a Wave.java

---
+++ 2D Arrays/Print Like a Wave.java
@@ -0,0 +1,26 @@
+public class Solution 
+{
+    public static void wavePrint(int mat[][])
+    {
+        int rows = mat.length;
+        int cols = 0;
+        if(rows!=0){
+            cols = mat[0].length;
+        }
+        for(int i = 0; i<cols; i++){
+            if (i%2==0){
+                for(int j = 0; j<rows; j++){
+                    System.out.print(mat[j][i] + " ");
+                }
+            }
+            else{
+
+                for(int j=rows-1;j>=0;j--){
+                    System.out.print(mat[j][i] + " ");
+                }
+            }
+
+        }
+    }
+
+}
