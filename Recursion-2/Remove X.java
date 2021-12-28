f393c424deda88f47db8e97428435390f5bc6252
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2021-12-28 23:34:26 +0530

    Create Remove X.java

---
+++ Recursion-2/Remove X.java
@@ -0,0 +1,24 @@
+public class solution 
+{
+		public static String removeX(String str)
+        {
+		if(str.length()==0) 
+        {
+			return str;
+		}
+        String smallAns=removeX(str.substring(1));
+		if(str.charAt(0)=='x') {
+			return smallAns;
+		}
+		return str.charAt(0) + smallAns;
+	}
+
+	public static void main(String[] args) 
+    {
+	
+        String str="xxxaxbxcdsxexxxx";
+		System.out.println(removeX(str));
+
+	}
+
+}
