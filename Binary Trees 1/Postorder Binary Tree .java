d00dd89e27209d78685a63d243aca912adc38a36
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2022-01-06 22:00:07 +0530

    Create Postorder Binary Tree .java

---
+++ Binary Trees 1/Postorder Binary Tree .java
@@ -0,0 +1,14 @@
+public class Solution 
+{
+    public static void postOrder(BinaryTreeNode<Integer> root)
+    {
+        if (root == null)
+        {  
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+}
