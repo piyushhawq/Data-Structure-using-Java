eb603bcdf2a409609c11d3364579c65cc10aa106
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2022-01-07 15:10:55 +0530

    Create Nodes without sibling.java

---
+++ Binary Trees 1/Nodes without sibling.java
@@ -0,0 +1,35 @@
+public class Solution 
+{
+
+	public static void printNodesWithoutSibling(BinaryTreeNode<Integer> root) 
+    {
+		
+        if (root==null)
+        {
+            return; 
+        }
+        
+        if (root.left==null && root.right==null)
+        {
+            return;
+        }
+        
+        if (root.left==null)
+        {
+            System.out.print(root.right.data+" ");
+            printNodesWithoutSibling(root.right);
+        }
+        else if (root.right==null)
+        {
+            System.out.print(root.left.data+" ");
+            printNodesWithoutSibling(root.left);
+            
+        }
+        else
+        {
+            printNodesWithoutSibling(root.left);
+            printNodesWithoutSibling(root.right);
+        }
+	}
+
+}
