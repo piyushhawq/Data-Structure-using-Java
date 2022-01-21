50d80e09ad0ec77896f116d8ad09bc40c4b0e54f
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2022-01-21 20:48:32 +0530

    Update Return Array Sum.java

--- Arrays/Return Array Sum.java
+++ Arrays/Return Array Sum.java
@@ -16,3 +16,17 @@ public class Solution
 		System.out.println(sum(arr));
 	}
 }
+
+/////////
+public class Solution
+{
+	public static int sum(int[] arr)
+    {
+		int sum=0;
+         for(int i=0;i<arr.length;i++)
+        {
+          sum=sum+arr[i];
+        }
+        return sum;
+	}
+}
